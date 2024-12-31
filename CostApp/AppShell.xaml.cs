f41f8d5365ee327531b1ea63b6411698f1e6c2f9
Smail Melki <smailmelki@gmail.com>
2024-12-31 23:23:26 +0100

    Update Code

--- CostApp/AppShell.xaml.cs
+++ CostApp/AppShell.xaml.cs
@@ -2,9 +2,17 @@
 {
     public partial class AppShell : Shell
     {
+        public bool IsDarkMode { get; set; }
         public AppShell()
         {
             InitializeComponent();
+            IsDarkMode = Application.Current?.UserAppTheme == AppTheme.Dark;
+            BindingContext = this;
+        }
+
+        private void OnThemeSwitchToggled(object sender, ToggledEventArgs e)
+        {
+            Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
         }
     }
 }
