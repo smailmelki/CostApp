92a59e8e06e775af57dd29ddfd8359d098cc15fa
Smail Melki <smailmelki@gmail.com>
2025-01-01 14:13:26 +0100

    add mode

--- CostApp/AppShell.xaml.cs
+++ CostApp/AppShell.xaml.cs
@@ -1,18 +1,33 @@
-﻿namespace CostApp
+﻿#if ANDROID
+using Android.Content.Res;
+#endif
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace CostApp
 {
     public partial class AppShell : Shell
     {
-        public bool IsDarkMode { get; set; }
+        public bool IsLightMode { get; set; }
         public AppShell()
         {
             InitializeComponent();
-            IsDarkMode = Application.Current?.UserAppTheme == AppTheme.Dark;
+            mode();
             BindingContext = this;
         }
 
         private void OnThemeSwitchToggled(object sender, ToggledEventArgs e)
         {
-            Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+            Application.Current.UserAppTheme = e.Value ? AppTheme.Light : AppTheme.Dark;
+        }
+
+        void mode()
+        {
+#if ANDROID
+            var uiModeFlags = Android.App.Application.Context?.Resources?.Configuration?.UiMode & UiMode.NightMask;
+                IsLightMode = uiModeFlags == UiMode.NightNo;
+#else
+            IsLightMode = Application.Current?.RequestedTheme == AppTheme.Light;
+#endif
         }
     }
 }
